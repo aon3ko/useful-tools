01b411db23db0bb800e3e419979cdbae30d0919b
Arreta <arreta@dawn.moe>
2026-05-21 13:57:24 +1000

    kh.sh: Indentation by Tab

--- kh.sh
+++ kh.sh
@@ -3,29 +3,29 @@
 file="$HOME/.ssh/known_hosts"
 
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 <line> [line ...]" >&2
-  exit 1
+	echo "Usage: $0 <line> [line ...]" >&2
+	exit 1
 fi
 if [ ! -f "$file" ]; then
-  echo "File not found: $file" >&2
-  exit 1
+	echo "File not found: $file" >&2
+	exit 1
 fi
 
 script=
 for parameter do
-  case "$parameter" in
-    ''|*[!0-9]*)
-      echo "Line must be a positive integer: $parameter" >&2
-      exit 1
-      ;;
-  esac
+	case "$parameter" in
+		''|*[!0-9]*)
+			echo "Line must be a positive integer: $parameter" >&2
+			exit 1
+			;;
+	esac
 
-  if [ "$parameter" -lt 1 ]; then
-    echo "Line must be a positive integer: $parameter" >&2
-    exit 1
-  fi
+	if [ "$parameter" -lt 1 ]; then
+		echo "Line must be a positive integer: $parameter" >&2
+		exit 1
+	fi
 
-  script="${script}${parameter}d;"
+	script="${script}${parameter}d;"
 done
 
 sed -i "$script" "$file"
